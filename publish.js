1b5db4f07898f9b36eba2bdd4d2b968dc837a1d4
Karoline Setoue (Adecco Recursos Humanos SA) <v-kaseto@microsoft.com>
2018-04-09 16:13:55 -0300

    First commit

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../botkarol.zip');
+var kuduApi = 'https://botkarol.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$botkarol';
+var password = 'y89qzCji4RpapKbqoeHPaa5jJawHgTwxgP9KSYAGwQDPe2r6HPptBlx7FNT3';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('botkarol publish');
+  } else {
+    console.error('failed to publish botkarol', err);
+  }
+});
